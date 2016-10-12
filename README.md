2960033219241b435d907462aeca76f7c1662f37
VirginieGodfrin <virginiegodfrin@gmail.com>
2016-10-12 15:50:30 +0200

    first entity : Album

---
+++ README.md
@@ -0,0 +1,4 @@
+Doctrine_relationship
+=====================
+
+A Symfony project created on October 12, 2016, 10:20 am.
---
+++ src/AppBundle/Entity/Album.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Album
+ *
+ * @ORM\Table(name="album")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AlbumRepository")
+ */
+class Album
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isPublish", type="boolean")
+     */
+    private $isPublish;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Album
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Album
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set isPublish
+     *
+     * @param boolean $isPublish
+     *
+     * @return Album
+     */
+    public function setIsPublish($isPublish)
+    {
+        $this->isPublish = $isPublish;
+
+        return $this;
+    }
+
+    /**
+     * Get isPublish
+     *
+     * @return bool
+     */
+    public function getIsPublish()
+    {
+        return $this->isPublish;
+    }
+}
+
